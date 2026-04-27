9002e6b068e71d7f6ed4886b1421983feb60ac59
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-04-27 23:10:51 +0100

    refactor: changes relative imports to `@`

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { LandingNavbar } from "../components/landing/LandingNavbar";
-import { HeroSection } from "../components/landing/HeroSection";
-import { StatsBar } from "../components/landing/StatsBar";
-import { HowItWorks } from "../components/landing/HowItWorks";
-import { FeaturesGrid } from "../components/landing/FeaturesGrid";
-import { ForWhom } from "../components/landing/ForWhom";
-import { Testimonials } from "../components/landing/Testimonials";
-import { CtaSection } from "../components/landing/CtaSection";
+import { LandingNavbar } from "@/components/landing/LandingNavbar";
+import { HeroSection } from "@/components/landing/HeroSection";
+import { StatsBar } from "@/components/landing/StatsBar";
+import { HowItWorks } from "@/components/landing/HowItWorks";
+import { FeaturesGrid } from "@/components/landing/FeaturesGrid";
+import { ForWhom } from "@/components/landing/ForWhom";
+import { Testimonials } from "@/components/landing/Testimonials";
+import { CtaSection } from "@/components/landing/CtaSection";
 
 export const Route = createFileRoute("/")({
   component: LandingPage,
